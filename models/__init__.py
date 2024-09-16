1c85f2393a0d043da421e1e1c5f097a596328350
anas1IA-art <anasimproveai@gmail.com>
2024-09-16 13:27:10 +0100

    I'm trying to scripting all models of recommendations systems

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .autorec import*
+from .caser import*
+from .deepfm import*
+from .fm import*
+from .mf import*
+from .neumf import*
+from .base_model import*
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='system_recommandations_suitables_models',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        
+    ],
+    # Metadata
+    author='Anas Nouri',
+    author_email='anas.nouri@usmba.ac.ma',
+
+)
